750479980535f1bd65d37cc8c74ab5fdf368c628
Best <bestsakditat@gmail.com>
2026-02-13 15:46:05 +0700

    refactor: update import paths for connection pool and middleware in auth routes

--- middlewares/protectAdmin.mjs
+++ middlewares/protectAdmin.mjs
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-import connectionPool from "../utils/db.mjs";
+import { connectionPool } from "../utils/db.mjs";
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
